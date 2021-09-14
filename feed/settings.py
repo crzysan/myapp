75acd99dca72b9695dcd8aac673f10250b8b3eaf
crzysan <christian.candelaria@hotmail.com>
2021-09-14 16:06:28 +0200

    Update health check

--- feed/settings.py
+++ feed/settings.py
@@ -47,8 +47,8 @@ INSTALLED_APPS = [
     'health_check.cache',
     'health_check.storage',
     'health_check.contrib.migrations',
-    'health_check.contrib.celery',              # requires celery
-    'health_check.contrib.celery_ping',         # requires celery
+#    'health_check.contrib.celery',              # requires celery
+#    'health_check.contrib.celery_ping',         # requires celery
     'health_check.contrib.psutil',              # disk and memory utilization; requires psutil 
 ]
 
--- feed/urls.py
+++ feed/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('feedapp.urls')),
     path('', include('social_django.urls')),
-    url(r'^ht/', include('health_check.urls')),
+    path('ht/', include('health_check.urls')),
 ]

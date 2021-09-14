d2095f64f886429eb0ae51db240aca5f206569d8
crzysan <christian.candelaria@hotmail.com>
2021-09-14 13:40:18 +0200

    Turn of DEBUG in settings and added k8s

--- feed/settings.py
+++ feed/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#k%tlnn)qokkdd&3n2xbp-d^4n-v%ku3+px8e^ugm6rka=+wi5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['192.168.0.125', '94.227.244.125', 'localhost', '127.0.0.1']
+DEBUG = False
+ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = ['192.168.0.125', '94.227.244.125', 'localhost', '127.0.0.1']
 
 
 # Application definition

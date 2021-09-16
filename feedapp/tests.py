15eb5ddd6a0b06bab927feebe5f9c47d05a55b71
crzysan <christian.candelaria@hotmail.com>
2021-09-16 11:13:06 +0200

    add unit test

--- feedapp/tests.py
+++ feedapp/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.test.client import Client
+
+# store the password to login later
+PASSWORD = 'mypassword' 
+USERNAME = 'admin'
+EMAIL = 'admin@myapp.com'
+
+User = get_user_model()
+ADMIN_USER = User.objects.create_superuser('$USERNAME', '$EMAIL', '$PASSWORD')
+
 
 # Create your tests here.
+class UserTestCase(TestCase):
+    def setUp(self):
+        User.objects.create_superuser('$USERNAME', '$EMAIL', '$PASSWORD')
+
+    def test_animals_can_speak(self):
+        """Users that exists are correctly identified"""
+        c = Client()
+        admin_user = User.object.get(username="admin")
+        self.assertEqual(c.login(username=admin_user.username, password=admin_user.password), 'Admin user is logged in and says "Hello!"')
